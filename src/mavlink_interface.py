27efa63cdd39348b95af816541ac69084c105391
madhukarkalyan40-dev <madhukarkalyan40@gmail.com>
2026-07-17 19:46:15 +0530

    Update mavlink_interface.py

--- src/mavlink_interface.py
+++ src/mavlink_interface.py
@@ -1,1 +1,17 @@
+"""
+MAVLink Communication Interface
+"""
 
+class MAVLinkConnection:
+
+    def connect(self):
+        print("Connecting to Pixhawk...")
+
+    def heartbeat(self):
+        print("Heartbeat Received")
+
+
+if __name__ == "__main__":
+    mav = MAVLinkConnection()
+    mav.connect()
+    mav.heartbeat()
