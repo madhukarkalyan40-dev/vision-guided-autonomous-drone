16f449701e9c61ed3e6e80fdcab2d99249c8ba12
madhukarkalyan40-dev <madhukarkalyan40@gmail.com>
2026-07-17 19:45:48 +0530

    Update obstacle_detection.py

--- src/obstacle_detection.py
+++ src/obstacle_detection.py
@@ -1,1 +1,17 @@
+class ObstacleDetector:
 
+    def __init__(self):
+        self.safe_distance = 3
+
+    def detect(self, distance):
+
+        if distance < self.safe_distance:
+            print("Obstacle Detected")
+            print("Changing Flight Path")
+        else:
+            print("Path Clear")
+
+
+if __name__ == "__main__":
+    detector = ObstacleDetector()
+    detector.detect(2)
